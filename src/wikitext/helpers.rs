e52bce8cbea214fa32b4dca2a4db9b245a30c7b8
Colin Snover <github.com@zetafleet.com>
2026-03-28 16:19:15 -0500

    Do not extract text from category pseudo-links
    This fixes the outline links on pages where a heading contains a
    category tag.

--- src/wikitext/helpers.rs
+++ src/wikitext/helpers.rs
@@ -1,6 +1,10 @@
 //! Wikitext parser helpers.
 
-use super::{Span, visit::Visitor};
+use super::{
+    Argument, InclusionMode, Span, Spanned, Token,
+    visit::{Visitor, visit_link},
+};
+use crate::title::{Namespace, Title};
 use core::fmt;
 
 /// Extracts all text from a token tree.
@@ -42,13 +46,36 @@ where
     fn visit_generated(&mut self, _span: Span, text: &'tt str) -> fmt::Result {
         self.content.write_str(text)
     }
+    fn visit_link(
+        &mut self,
+        span: Span,
+        target: &'tt [Spanned<Token>],
+        content: &'tt [Spanned<Argument>],
+        trail: Option<&'tt str>,
+    ) -> Result<(), fmt::Error> {
+        // TODO: Actually evaluate the target (which requires making this helper
+        // capable of evaluating wikitext, which is annoying, but then again,
+        // Title is going to need to access the site configuration from memory
+        // eventually instead of getting it from static memory, which is going
+        // to mean this visitor needs access to the runtime state globals
+        // anyway).
+        #[rustfmt::skip]
+        if let [Spanned { span, node: Token::Text }] = target {
+            let title = Title::new(&self.source[span.into_range()], None);
+            if title.namespace().id == Namespace::CATEGORY {
+                return Ok(());
+            }
+        };
+
+        visit_link(self, span, target, content, trail)
+    }
     fn visit_new_line(&mut self, _span: Span) -> fmt::Result {
         self.content.write_char(' ')
     }
     fn visit_text(&mut self, text: &str) -> fmt::Result {
         self.content.write_str(text)
     }
-    fn visit_start_include(&mut self, _span: Span, _mode: super::InclusionMode) -> fmt::Result {
+    fn visit_start_include(&mut self, _span: Span, _mode: InclusionMode) -> fmt::Result {
         todo!("inclusion control in text extractor")
     }
 }
